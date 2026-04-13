9c959aefc712af56596e27d48c4c0cff4811e025
Manojkumar <manojkumar26198@gmail.com>
2026-04-13 19:48:12 +0530

    fix: add app folder with Dockerfile and Flask app

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import os, socket
+
+app = Flask(__name__)
+VERSION = os.getenv("APP_VERSION", "1.0.0")
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Blue/Green Deployment Demo",
+        "version": VERSION,
+        "host": socket.gethostname(),
+        "env": os.getenv("APP_ENV", "dev")
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
